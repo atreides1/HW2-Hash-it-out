a161ed945e97f2799b88887895958b54428e3108
Mercy Bhakta <mercybhakta@yahoo.com>
2018-10-11 12:47:12 -0700

    Created a cache object to test

--- test.cc
+++ test.cc
@@ -1,18 +1,23 @@
 #include "Cache0.cc"
 #include <iostream>
-
+#include <string>
 uint32_t maxmem = 1024;
 
-void evictor()
+int evictor()
 {
+        return 0;
 }
 
 
 //test: create a cache and insert key
 int test_insert()
 {
-	//Cache c(maxmem, evictor, std::hash); //create cache object
-	return 0;
+        Cache c(maxmem, evictor, std::hash<std::string>()); //create cache object
+        //string key = "a";
+        //char * val = "z";
+        //uint32_t size = 1;
+        //c.set(key, val, size);
+        return 0;
 
 }
 
@@ -22,8 +27,8 @@ int test_insert()
 //test: insert key and delete
 //test: query key that was evicted
 
-int main() 
+int main()
 {
-	test_insert();
-	return 0;
+        test_insert();
+        return 0;
 }
