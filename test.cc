c71ef4eb89a24f6b01b0dc141fec46fcd09c71d4
Mercy Bhakta <mercybhakta@yahoo.com>
2018-10-11 23:10:47 -0700

    Added more tests
    Had to copy and paste - trying to "get" a key that wasn't inserted throws an error - need to fix in Cache0.cc

--- test.cc
+++ test.cc
@@ -1,6 +1,6 @@
-#include "Cache0.cc"
+#include "cache.hh"
 #include <iostream>
-#include <string>
+//#include <string>
 uint32_t maxmem = 1024;
 
 int evictor()
@@ -12,23 +12,70 @@ int evictor()
 //test: create a cache and insert key
 int test_insert()
 {
+        std::cout << "Create cache and insert key:" << '\n';
         Cache c(maxmem, evictor, std::hash<std::string>()); //create cache object
-        //string key = "a";
-        //char * val = "z";
-        //uint32_t size = 1;
-        //c.set(key, val, size);
+        std::string key = "a";
+        char val[] = "z";
+        uint32_t size = 1;
+        c.set(key, val, size);
+        std::cout << val << '\n';
         return 0;
 
 }
 
 //test: query key
+int test_query()
+{
+        std::cout << "Query key:" << '\n';
+        Cache c(maxmem, evictor, std::hash<std::string>()); //create cache object
+        std::string key = "a";
+        char val[] = "z";
+        uint32_t size = 1;
+        c.set(key, val, size);
+        std::cout << val << '\n';
+        //assert goes here
+        std::cout << c.get(key, size) << '\n';
+        return 0;
+
+}
 //test: query key that wasn't inserted
+//this one breaks
+int test_query_uninserted()
+{
+        std::cout << "Query key that wasn't inserted:" << '\n';
+        Cache c(maxmem, evictor, std::hash<std::string>()); //create cache object
+        std::string key = "a";
+        char val[] = "z";
+        uint32_t size = 1;
+        c.set(key, val, size);
+        std::cout << c.get("b", size) << '\n';
+        return 0;
+}
+
 //test: insert key and modify
+int test_insert_and_mod()
+{
+        std::cout << "Insert key and modify value:" << '\n';
+        Cache c(maxmem, evictor, std::hash<std::string>()); //create cache object
+        std::string key = "a";
+        char val[] = "z";
+        char val2[] = "y";
+        uint32_t size = 1;
+        c.set(key, val, size);
+        std::cout << "Original value: " << c.get(key, size) << '\n';
+        c.set(key, val2, size);
+        std::cout << "New value: " << c.get(key, size) << '\n';
+        return 0;
+
+}
 //test: insert key and delete
 //test: query key that was evicted
 
 int main()
 {
         test_insert();
+        test_query();
+        //test_query_uninserted();
+        test_insert_and_mod();
         return 0;
 }
