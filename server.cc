ab4d5d6a0093584b4f9aa74f98582581a8a18db2
sschlott <43354996+sschlott@users.noreply.github.com>
2018-11-11 11:12:52 -0800

    Added other funcs from proj desc

--- server.cc
+++ server.cc
@@ -16,41 +16,78 @@ int main(int argc, char *argv[])
     SimpleApp app;
 /*
 Need 
-set(key,val, size)
-get(key, pointer to val)
-delete(key)
-space_used
-max_em/
+**GET /key/k: Returns a JSON tuple with { key: k, value: value: v } 
 
+**PUT /key/k/v: create or replace a k,v   //IS THIS THE SAME? 
+**DELETE /key/k
+**HEAD /key/k: Return just a header, regardless of the resource (key) requested
 
 If size null, it's get or del
 If val and size null it's del
 if all parameters, it's set? 
 */
-    CROW_ROUTE(app, "/cache/<string>/<string>/<string>/<int>")
-        .methods("SET"_method, "GET"_method, "DELETE"_method)
-        ([](const request& req, std::string call, std::string key, std::string val, int size) {
-            if (req.method == "SET"_method)
+    CROW_ROUTE(app, "/key/<string>/<string>")
+        .methods("GET"_method, "PUT"_method, "DELETE"_method, "HEAD"_method)
+        ([](const request& req, std::string key, std::string val) {
+            if (req.method == "GET"_method)
             {
                 if ((req.url_params.get("v") != nullptr) & (req.url_params.get("q") != nullptr))
                 {
                     // ...
                 }
-                return response(200, "You used SET");
-            }
-            else if (req.method == "GET"_method)
-            {
                 return response(200, "You used GET");
+            }
+            else if (req.method == "PUT"_method)
+            {
+                return response(200, "You used PUT");
             }
             else if (req.method == "DELETE"_method)
             {
                 return response(200, "You used DELETE");
             }
+            else if (req.method == "HEAD"_method)
+            {
+                return response(200, "You used HEAD");
+            }
             else
             {
                 return response(404);
-            }  
-	   });
+            }
+        });
+//GET /memsize: Returns a JSON tuple with { memused: mem }
+    CROW_ROUTE(app, "/memsize")
+        .methods("GET"_method)
+        ([](const request& req) {
+            if (req.method == "GET"_method)
+            {
+                if ((req.url_params.get("v") != nullptr) & (req.url_params.get("q") != nullptr))
+                {
+                    // ...
+                }
+                return response(200, "You used GET");
+            }
+            else
+            {
+                return response(404);
+            }
+        });
+//POST /shutdown: Upon receiving this message, the server stops accepting requests
+    CROW_ROUTE(app, "/shutdown")
+        .methods("POST"_method)
+        ([](const request& req) {
+            if (req.method == "POST"_method)
+            {
+                if ((req.url_params.get("v") != nullptr) & (req.url_params.get("q") != nullptr))
+                {
+                    // ...
+                }
+                return response(200, "You used POST");
+            }
+            else
+            {
+                return response(404);
+            }
+        });
     app.port(portnum).run();
 }
     
