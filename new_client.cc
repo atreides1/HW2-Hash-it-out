20e6eabc92466e9be0a15e2a9b331bcdc0bb979e
vagrant <schlotts@reed.edu>
2018-11-15 18:55:36 +0000

    Trying out cout

--- new_client.cc
+++ new_client.cc
@@ -99,6 +99,7 @@ public:
 				fprintf(stderr, "curl_easy_perform() failed: %s\n",
              			curl_easy_strerror(res));
  
+			std::cout >> res;	
 			    /* always cleanup */ 
     			curl_easy_cleanup(curl_);
   		}
